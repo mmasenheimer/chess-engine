e2deb4f1eb3810cd2e3d097925030a4f24498959
mmasenheimer <mmasenheimer21@gmail.com>
2025-07-08 12:59:51 -0700

    Initial commit

---
+++ Chess/ChessEngine.py
@@ -0,0 +1,4 @@
+'''
+This class is responsible for storing info about the current game state,
+it's also responsible for determining the legality of moves and updating the game state.
+'''
---
+++ Chess/ChessMain.py
@@ -0,0 +1,4 @@
+'''
+This is the main driver file, which is responsible for
+handling user input and displaying the current game state.
+'''
