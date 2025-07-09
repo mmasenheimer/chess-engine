8dda23a4f39854601407f0abba9d163e7b6cf01c
mmasenheimer <mmasenheimer21@gmail.com>
2025-07-09 02:27:17 -0700

    Added move feature

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -28,7 +28,7 @@ def load_images():
 
 def main():
     p.init()
-    screen = p.display.set_mode((WIDTH, HEIGHT))
+    screen = p.display.set_mode((WIDTH, HEIGHT), p.NOFRAME, p.RESIZABLE)
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
 
@@ -38,15 +38,42 @@ def main():
     load_images()
 
     running = True
+    sqSelected = ()
+    # This to keep track of the last square selected
+    playerClicks = []
+    # Keep track of clicks (2 tuples: [(6, 4), (4, 4)])
 
     while running:
         for e in p.event.get():
 
             if e.type == p.QUIT:
                 running = False
+            
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                # Get the xy position of the mouse click
+
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
+
+                if sqSelected == (row, col):
+                    # If the same square is clicked again, deselect it
+                    sqSelected = ()
+                    playerClicks = []
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)
+                
+                if (len(playerClicks) == 2):
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.make_move(move)
+                    sqSelected = ()
+                    # Reset the player clicks after a move is made
+                    playerClicks = []
+
 
             draw_game_state(screen, gs)    
-            
             clock.tick(MAX_FPS)
             p.display.flip()
 
@@ -80,7 +107,6 @@ def draw_pieces(screen, board):
                 screen.blit(IMAGES[piece], p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
     
-
 
 if __name__ == "__main__":
     main()
